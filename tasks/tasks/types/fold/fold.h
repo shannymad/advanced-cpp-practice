045b3bc6fd86d056b8f5524133ef4bba6b33e110
Софья Полянская <svpolianskaia@edu.hse.ru>
2026-09-05 20:12:12 +0300

    solved fold task

--- tasks/tasks/types/fold/fold.h
+++ tasks/tasks/types/fold/fold.h
@@ -1,13 +1,56 @@
 #pragma once
 
-struct Sum {};
+struct Sum {
 
-struct Prod {};
+    template<typename T, typename U>
+    auto operator() (const T& a, const U& b) const {
+        return a + b;
+    }
 
-struct Concat {};
+};
+
+struct Prod {
+    template<typename T, typename U>
+    auto operator() (const T& a, const U& b) const {
+        return a * b;
+    }
+};
+
+struct Concat {
+    template <typename T, typename U>
+    auto operator() (T a1, const U& a2) const {
+        a1.insert(a1.end(), a2.begin(), a2.end());
+        return a1;
+    }
+};
 
 template <class Iterator, class T, class BinaryOp>
 T Fold(Iterator first, Iterator last, T init, BinaryOp func) {
+    while (first != last) {
+        init = func(init, *first);
+        ++first;
+
+    }
+    return init;
+
+
 }
 
-class Length {};
+class Length {
+    public: 
+
+    explicit Length(int* length): length_(length){
+        *length_ = 0;
+
+    }
+
+    template <typename T, typename U>
+    T operator() (T init, const U&)const {
+        ++(*length_);
+        return init;
+    }
+
+
+    private:
+    int* length_;
+};
